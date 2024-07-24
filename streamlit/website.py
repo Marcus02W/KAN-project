c9ba197f5fcc8d3d1fefcd632ad44ea65ff191aa
FabianDHBW <s212368@student.dhbw-mannheim.de>
2024-07-24 23:04:34 +0200

    added more models

--- streamlit/website.py
+++ streamlit/website.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4278fd1281f97ea591899014979ced8979e8acaa93eb72da6e4897c2e46a8cfc
-size 19231
+oid sha256:86aa3cfa97dec2c2d919bf0a5ba85a0ecbb1f2777269dcc4a15071c217021031
+size 19285
